b768c5eee21e9ef50dad146aa2dd5943c77fa4e4
zerodiverse <59115514+zerodiversex@users.noreply.github.com>
2022-09-26 09:34:45 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}//read-guestbook-entries-sequence.json`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
